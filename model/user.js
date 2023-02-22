de2c03ebdfc681478206e21ad7d5033c8deead4a
veronicahWambui <njengaveronicahwambui@gmail.com>
2023-02-22 17:46:55 +0300

    intial

---
+++ model/user.js
@@ -0,0 +1,12 @@
+
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  first_name:{type : 'string',default: null},
+  last_name: { type: String, default: null },
+  email: { type: String, unique: true },
+  password: { type: String },
+  token: { type: String },
+
+});
+module.exports = model("user", userSchema);
